c4d79c28e4b201b61206ba26bc62a470e6bad856
manjeetcodes <manjeetcodes@gmail.com>
2024-01-12 19:16:58 +0530

    LinkBox

--- website-summarizer/src/components/demo.jsx
+++ website-summarizer/src/components/demo.jsx
@@ -1,10 +1,47 @@
 
-
+// import { useState, useEffect } from "react";
+// import {copy, linkIcon, loader, tick } from "../assets";
+import { useState } from 'react';
+import '../App.css';
 const demo = () => {
+  const [article, setArticle]= useState({
+    url:'',
+    summary:''
+  })
+  const handleSubmit = async (e)=>{alert("Submitted")}
   return (
-    <div> 
-      
-    </div>
+    <section className="mt-16 w-full max-w-full ">
+      <div className="searchbox flex flex-col w-full gap-2 ">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          {/* <img 
+    src="{linkIcon}" 
+    alt="linkicon"
+    className="absolute left-0 my-2 ml-3 w-5 "
+     /> */}
+
+          <input type="url"
+            placeholder="Paste Link"
+            value={article.url}
+            onChange={(e) => {setArticle({
+              ... article, url:e.target.value
+            })}}
+            required
+            className="input"
+
+          />
+          <button type='submit'
+          // onClick={handleSubmit}
+          className="peer-focus:border-gray-600 peer-focus:text-gray-100  ">
+            ✦
+          </button>
+          
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- website-summarizer/src/components/hero.jsx
+++ website-summarizer/src/components/hero.jsx
@@ -10,12 +10,12 @@ const hero = () => {
                 <button
                     type='button'
                     onClick={() => window.open('https://github.com/manishindiyaar')}
-                    className='black-btn p-1 bg-slate-700 text-white rounded-sm'
+                    className='black-btn px-5 py-2 bg-slate-700 text-white rounded-xl'
                 >Github</button>
             </nav>
-            <h1 className='text-5xl font-black head_txt flex flex-col'>
+            <h1 className='text-5xl text-slate-700 font-black head_txt flex flex-col'>
                 Summarize Articles with <br />
-                <span className='font-black text-7xl bg-gradient-to-r from-indigo-500 '>OpenAI GPT 4</span>
+                <span className='font-black text-white text-7xl bg-gradient-to-r from-indigo-900 rounded-xl mb-2 '>OpenAI GPT 4</span>
             </h1>
             <h1 className="desc">
                 simplify your Reading with Beautiful summary
